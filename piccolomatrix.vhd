870241a130025c599d7ed109daae56ab802ca47d
Yash Motagi <33543044+motagiyash@users.noreply.github.com>
2018-08-02 00:27:31 +0200

    Update piccolomatrix.vhd

--- piccolomatrix.vhd
+++ piccolomatrix.vhd
@@ -1,5 +1,27 @@
+----------------------------------------------------------------------------------
+-- Company: Esigelec
+-- Engineer: Yash Motagi
+-- 
+-- Create Date:    18:18:17 07/20/2018 
+-- Design Name: 
+-- Module Name:    piccolomatrix - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
+use IEEE.NUMERIC_STD.all;
 
 entity piccolomatrix is
 
@@ -8,105 +30,71 @@ port( data_i_s0 : in std_logic_vector (3 downto 0);
 		data_i_s2 : in std_logic_vector (3 downto 0);
 		data_i_s3 : in std_logic_vector (3 downto 0);
 		
-		data_o_s0 : out std_logic_vector (3 downto 0);
-		data_o_s1 : out std_logic_vector (3 downto 0);
-		data_o_s2 : out std_logic_vector (3 downto 0);
-		data_o_s3 : out std_logic_vector (3 downto 0));
+		data_o_s4 : out std_logic_vector (3 downto 0);
+		data_o_s5 : out std_logic_vector (3 downto 0);
+		data_o_s6 : out std_logic_vector (3 downto 0);
+		data_o_s7 : out std_logic_vector (3 downto 0));
 end piccolomatrix;
 
 architecture Behavioral of piccolomatrix is
 
+signal x0,x1,x2,x3 : std_logic_vector (4 downto 0);
+signal polynomial : std_logic_vector (4 downto 0) := "10011";
+signal out_buffer_0,out_buffer_1,out_buffer_2,out_buffer_3 : std_logic_vector (4 downto 0);
+signal s0,s1,s2,s3 : unsigned (3 downto 0);
 begin
-	matrix_process_sbox0 : process (data_i_s0) is
-				begin
-				multiplication_case0 : case data_i_s0 is
-            when "0000"   => data_o_s0 <= "0000";
-            when "0001"   => data_o_s0 <= "0011";
-            when "0010"   => data_o_s0 <= "0001";
-            when "0011"   => data_o_s0 <= "0100";
-            when "0100"   => data_o_s0 <= "0001";
-            when "0101"   => data_o_s0 <= "0100";
-            when "0110"   => data_o_s0 <= "0010";
-            when "0111"   => data_o_s0 <= "0101";
-            when "1000"   => data_o_s0 <= "0010";
-            when "1001"   => data_o_s0 <= "0101";
-            when "1010"   => data_o_s0 <= "0011";
-            when "1011"   => data_o_s0 <= "0110";
-            when "1100"   => data_o_s0 <= "0011";
-            when "1101"   => data_o_s0 <= "0110";
-            when "1110"   => data_o_s0 <= "0100";
-            when "1111"   => data_o_s0 <= "0111";
-				when others => data_o_s0 <= "0000";
-			end case multiplication_case0;
-		end process matrix_process_sbox0;
-			
-	matrix_process_sbox1 : process (data_i_s1) is
-				begin
-				multiplication_case1 : case data_i_s1 is
-            when "0000"   => data_o_s1 <= "0000";
-            when "0001"   => data_o_s1 <= "0001";
-            when "0010"   => data_o_s1 <= "0001";
-            when "0011"   => data_o_s1 <= "0010";
-            when "0100"   => data_o_s1 <= "0010";
-            when "0101"   => data_o_s1 <= "0011";
-            when "0110"   => data_o_s1 <= "0011";
-            when "0111"   => data_o_s1 <= "0100";
-            when "1000"   => data_o_s1 <= "0011";
-            when "1001"   => data_o_s1 <= "0100";
-            when "1010"   => data_o_s1 <= "0100";
-            when "1011"   => data_o_s1 <= "0101";
-            when "1100"   => data_o_s1 <= "0101";
-            when "1101"   => data_o_s1 <= "0110";
-            when "1110"   => data_o_s1 <= "0110";
-            when "1111"   => data_o_s1 <= "0111";
-				when others => data_o_s1 <= "0000";
-			end case multiplication_case1;
-		end process matrix_process_sbox1;
-		
-	matrix_process_sbox2 : process (data_i_s2) is
-				begin
-				multiplication_case2 : case data_i_s2 is
-            when "0000"   => data_o_s2 <= "0000";
-            when "0001"   => data_o_s2 <= "0001";
-            when "0010"   => data_o_s2 <= "0010";
-            when "0011"   => data_o_s2 <= "0011";
-            when "0100"   => data_o_s2 <= "0011";
-            when "0101"   => data_o_s2 <= "0100";
-            when "0110"   => data_o_s2 <= "0101";
-            when "0111"   => data_o_s2 <= "0110";
-            when "1000"   => data_o_s2 <= "0001";
-            when "1001"   => data_o_s2 <= "0010";
-            when "1010"   => data_o_s2 <= "0011";
-            when "1011"   => data_o_s2 <= "0100";
-            when "1100"   => data_o_s2 <= "0100";
-            when "1101"   => data_o_s2 <= "0101";
-            when "1110"   => data_o_s2 <= "0110";
-            when "1111"   => data_o_s2 <= "0111";
-				when others => data_o_s2 <= "0000";
-			end case multiplication_case2;
-		end process matrix_process_sbox2;
-			
-	matrix_process_sbox3 : process (data_i_s3) is
-				begin
-				multiplication_case3 : case data_i_s3 is
-            when "0000"   => data_o_s3 <= "0000";
-            when "0001"   => data_o_s3 <= "0010";
-            when "0010"   => data_o_s3 <= "0011";
-            when "0011"   => data_o_s3 <= "0101";
-            when "0100"   => data_o_s3 <= "0001";
-            when "0101"   => data_o_s3 <= "0011";
-            when "0110"   => data_o_s3 <= "0100";
-            when "0111"   => data_o_s3 <= "0101";
-            when "1000"   => data_o_s3 <= "0001";
-            when "1001"   => data_o_s3 <= "0011";
-            when "1010"   => data_o_s3 <= "0100";
-            when "1011"   => data_o_s3 <= "0110";
-            when "1100"   => data_o_s3 <= "0010";
-            when "1101"   => data_o_s3 <= "0100";
-            when "1110"   => data_o_s3 <= "0101";
-            when "1111"   => data_o_s3 <= "0111";
-				when others => data_o_s3 <= "0000";
-			end case multiplication_case3;
-		end process matrix_process_sbox3;
+
+x_process : process (data_i_s0,data_i_s1,data_i_s2,data_i_s3) is
+begin
+x0<=((00010)*data_i_s0 xor (00010)*data_i_s1 + data_i_s1 xor data_i_s2 xor data_i_s3);
+x1<=(data_i_s0 xor (00010)*data_i_s1 xor (00010)*data_i_s2 + data_i_s2 xor data_i_s3);
+x2<=(data_i_s0 xor data_i_s1 xor (00010)*data_i_s2 xor (00010)*data_i_s3 + data_i_s3);
+x3<=((00010)*data_i_s0 + data_i_s0 xor data_i_s1 xor data_i_s2 xor (00010)*data_i_s3);
+end process x_process;
+
+update_process : process (x0,x1,x2,x3) is
+begin
+	if (x0(4)='1') then
+		out_buffer_0 <= x0 xor polynomial;
+	else 
+		out_buffer_0 <= x0;
+	end if;
+	if (x1(4)='1') then
+		out_buffer_1 <= x1 xor polynomial;
+	else 
+		out_buffer_1 <= x1;
+	end if;
+	if (x2(4)='1') then
+		out_buffer_2 <= x2 xor polynomial;
+	else 
+		out_buffer_2 <= x2;
+	end if;
+	if (x3(4)='1') then
+		out_buffer_3 <= x3 xor polynomial;
+	else 
+		out_buffer_3 <= x3;
+	end if;
+end process update_process;
+
+out_process_0 :process (out_buffer_0) is
+begin
+	data_o_s4 <= out_buffer_0(3 downto 0);
+end process out_process_0;
+
+out_process_1 :process (out_buffer_1) is
+begin
+	data_o_s5 <= out_buffer_1(3 downto 0);
+end process out_process_1;
+
+out_process_2 :process (out_buffer_2) is
+begin
+	data_o_s6 <= out_buffer_2(3 downto 0);
+end process out_process_2;
+
+out_process_3 :process (out_buffer_3) is
+begin
+	data_o_s7 <= out_buffer_3(3 downto 0);
+end process out_process_3;
+
 
 end Behavioral;
